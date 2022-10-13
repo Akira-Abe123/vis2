dfbbc17ef10eec6bb6949868821ddac9939d8ed0
Akira-Abe123 <111492220+Akira-Abe123@users.noreply.github.com>
2022-10-13 13:53:01 +1100

    Add files via upload

---
+++ vis2.js
@@ -0,0 +1,14 @@
+var vg_1 = "choropleth.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "worldline.json";
+vegaEmbed("#line", vg_2).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "bubbleplot.json";
+vegaEmbed("#bubble", vg_3).then(function(result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
